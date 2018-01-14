83e63fbf2fb333e049ef6522f09c2b991920c6ae
secretter <secretter@sec.com>
2018-01-14 11:42:08 +0900

    .

--- vars/tess.groovy
+++ vars/tess.groovy
@@ -1,14 +1,11 @@
 def call() {
-  properties([parameters([[$class: 'ExtensibleChoiceParameterDefinition', choiceListProvider: [$class: 'TextareaChoiceListProvider', addEditedValue: false, choiceListText: '''pjh
-omoo
-'''], description: '', editable: true, name: 'oikoi']])])
 
   pipeline {
     agent any
     stages {
       stage ('jojojo') {
         steps {
-          echo oikoi
+          echo params.ver
         }
       }
     }
