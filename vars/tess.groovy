4c63a07431582adaa6338168fa3011f45ee386c2
secretter <secretter@sec.com>
2018-01-14 12:15:46 +0900

    no message

--- vars/tess.groovy
+++ vars/tess.groovy
@@ -9,6 +9,8 @@ def call() {
       stage ('jojojo') {
         steps {
           echo params.ver
+          sh 'env'
+          sh 'java -jar jenkins-cli.jar help'
           script {
               sh 'pwd'
               sh 'ls -ltra'
