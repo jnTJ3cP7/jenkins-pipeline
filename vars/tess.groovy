23b7fc9e95ef8e03a13fc777c8e972c4084a8c82
secretter <secretter@sec.com>
2018-01-14 13:45:28 +0900

    I

--- vars/tess.groovy
+++ vars/tess.groovy
@@ -28,15 +28,18 @@ def call() {
 def doNonScp() {
   def jobName = 'ex8'
   def ins = Jenkins.getInstance()
-  def job = ins.getItemByFullName(jobName, AbstractItem)
-  def configXml = new File("${env.JENKINS_HOME}/config.xml.test").text
-  def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
+  // def job = ins.getItemByFullName(jobName, AbstractItem)
+  // def configXml = new File("${env.JENKINS_HOME}/config.xml.test").text
+  // def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
 
-  if (job) {
-    job.updateByXml(xmlStream);
-  }
-  else {
-    ins.createProjectFromXML(jobName, xmlStream)
+  // if (job) {
+  //   job.updateByXml(xmlStream);
+  // }
+  // else {
+  //   ins.createProjectFromXML(jobName, xmlStream)
+  // }
+  ins.getAllItems(AbstractItem).each {
+    echo it.fullName
   }
 
 }
